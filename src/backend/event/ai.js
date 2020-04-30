314d7ae71e6454243d289b38bc87c0aab009e87a
cage <dscheinah@googlemail.com>
2020-04-30 21:54:48 +0200

    add another optional AI

--- src/backend/event/ai.js
+++ src/backend/event/ai.js
@@ -3,7 +3,7 @@ const state = require('../state.js');
 module.exports = class Ai extends require('../lib/handler.js') {
     handle() {
         let required = state.requiredPlayers - state.getActivePlayers().length;
-        ['Alpha', 'Omega'].forEach((name) => {
+        ['Alpha', 'Omega', 'µν'].forEach((name) => {
             let player = state.createPlayer(name);
             if (!player.active && required > 0) {
                 required--;
