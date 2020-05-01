72186430db388a72cd5cfd4e6eeaadd22fb5428b
cage <dscheinah@googlemail.com>
2020-05-01 23:11:17 +0200

    do not create not required ai players

--- src/backend/event/ai.js
+++ src/backend/event/ai.js
@@ -4,8 +4,11 @@ module.exports = class Ai extends require('../lib/handler.js') {
     handle() {
         let required = state.requiredPlayers - state.getActivePlayers().length;
         ['Alpha', 'Omega', 'µν'].forEach((name) => {
+            if (required <= 0) {
+                return;
+            }
             let player = state.createPlayer(name);
-            if (!player.active && required > 0) {
+            if (!player.active) {
                 required--;
                 player.aiControlled();
             }
